6be63d82af908f62d6066cd0cd68afaf495c7bf5
anvaka <anvaka@gmail.com>
2014-09-23 03:30:25 -0700

    No eyes

--- main.js
+++ main.js
@@ -2,7 +2,3 @@ require('sillyheader-theme');
 var HeaderComponent = require('sillyheader');
 
 var eye = new HeaderComponent(document.body);
-
-document.body.addEventListener('mousemove', function (e) {
-  eye.lookAt(e.clientX, e.clientY);
-}, false);
